62dba72d8c469a6c4a71963945b55a3d05b6fb3b
Artem Shevchenko <ashevcen@gmail.com>
2024-05-30 17:44:34 +0300

    added 404 page and redirect

---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+function NotFound() {
+  return (
+    <div className="flex-center h-screen flex-col gap-y-5">
+      <h1 className="text-5xl font-bold">404</h1>
+
+      <p className="text-gray-900">Страница не найдена</p>
+
+      <Link to="/">
+        <button
+          type="button"
+          className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          Главная страница
+        </button>
+      </Link>
+    </div>
+  );
+}
+export default NotFound;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,16 +1,22 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Main from "./Main";
 import First from "./First";
+import NotFound from "./NotFound";
 
 function MainRouter() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Main />,
+      ErrorBoundary: () => <div>Error</div>,
     },
     {
       path: "/first",
       element: <First />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
     },
   ]);
 
