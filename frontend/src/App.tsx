89fc8905415c171652e06962bc2e18fbc208e623
Artem Shevchenko <ashevcen@gmail.com>
2024-05-30 13:07:13 +0300

    init frontend app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,35 @@
+function App() {
+  return (
+    <div className="flex-center h-screen flex-col gap-y-5">
+      <button
+        type="button"
+        className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      >
+        Авторизация
+      </button>
+
+      <button
+        type="button"
+        className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      >
+        Регистрация
+      </button>
+
+      <button
+        type="button"
+        className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      >
+        Обновление
+      </button>
+
+      <button
+        type="button"
+        className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      >
+        Страница 1
+      </button>
+    </div>
+  );
+}
+
+export default App;
